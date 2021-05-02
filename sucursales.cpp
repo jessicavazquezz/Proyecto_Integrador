d95dd6cabee4d1d93a10c8075695fc3a67c80e63
Diegosaroc <83524531+Diegosaroc@users.noreply.github.com>
2021-05-01 22:57:53 -0500

    Update sucursales.cpp

--- sucursales.cpp
+++ sucursales.cpp
@@ -1,52 +1,110 @@
 #include <string>
-#include "Sucursales.h"
+#include "Sucursales.hpp"
+#include <iostream>
 using namespace std;
 
 Sucursales::Sucursales(){
-  sucursal = new sucursal[1];
-  s = 1;
-  sizeS = 1;
-  nombre1 = "Sucursal 1";
-  nombre2 = "Sucursal2";
+    sucursal = new Sucursales[100];
+    nombresucursal = "";
+    direccionsucursal= "";
+    cantidadempleados = 0;
+    cantidadproductos = 0;
+    
+    
 }
 
+Sucursales::Sucursales(string noms, string dirs, int cane, int canp){
+    nombresucursal = noms;
+    direccionsucursal= dirs;
+    cantidadempleados = cane;
+    cantidadproductos = canp;
+    
+}
 
-void Sucursales::agregar(int s){
-  return sucursal;
-
+void Sucursales::setnombresucursal(string noms){
+    nombresucursal = noms;
+}
+void Sucursales::setdireccionsucursal(string dirs){
+    direccionsucursal= dirs;
+}
+void Sucursales::setcantidadempleados(int cane){
+    cantidadempleados = cane;
+}
+void Sucursales::setcantidadproductos(int canp){
+    cantidadproductos = canp;
+}
+string Sucursales::getnombresucursal(){
+    return nombresucursal;
+}
+string Sucursales::getdireccionsucursal(){
+    return direccionsucursal;
+}
+int Sucursales::getcantidadempleados(){
+    return cantidadempleados;
+}
+int Sucursales::getcantidadproductos(){
+    return cantidadproductos;
+}
+void Sucursales::agregar(){
+    int dato0= sizeof(sucursal)+1;
+    string dato1 = nombresucursal;
+    string dato2= direccionsucursal;
+    int dato3= cantidadempleados;
+    int dato4= cantidadempleados;
+    sucursal[dato0] = [dato1, dato2, dato3, dato4];
 }
 
 void Sucursales::modificar(){
-  for(int i = 0, i < 2, i++){
-    if (sucursales[i] == 1 || sucursales[i] == 2){
+    for(int i = 0; i < 2; i++){
+      if (sucursal[i] == 1 || sucursales[i] == 2){
+      }
     }
+  return sucursal;
   }
-return sucursal;
 }
-
 void Sucursales::borrar(){
-  for(int i = 0, i < 2, i++){
-    sucursal[i] == sucursal -1;
-  }
-return sucursal;
+    int dato0= sizeof(sucursal)-1;
+    string dato1 = nombresucursal;
+    string dato2= direccionsucursal;
+    int dato3= cantidadempleados;
+    int dato4= cantidadempleados;
+    sucursal[dato0] = [dato1, dato2, dato3, dato4];
 }
-
 void Sucursales::asociaE(){
-  Empleadoas = 0
-  for(sucursal[i], Empleadoas ++){
-    Empleadoas = Empleadoas + 1;
+    Empleadoas = 0
+    for(sucursal[i], Empleadoas ++){
+      Empleadoas = Empleadoas + 1;
+    }
+  return Empleadoas;
   }
-return Empleadoas;
 }
+void Sucursales::transferir(){
+    int Empleado = 0;
+    for(int i = 0; i < 2; i++){
+        for(int j=0; i<2; i++){
+            if(sucursal[i] != sucursal[j]){
+                Empleado = 1;
+            }
+        }
+        if(Empleado > 0 ){
+            cantidadempleados +1;
+        }
 
-string Sucursales::getnombreSucursal(int n){
-  if(int n = 1){
-    return nombre1;
-  }
-  if(int n = 2){
-    return nombre2;
-  }
-  else(){
-    cout << "La sucursal no existe";
-  }
+            
 }
+    
+void Sucursales::editarprodcuto(){
+    int Empleado = 0;
+    for(int i = 0; i < 2; i++){
+        for(int j=0; i<2; i++){
+            if(sucursal[i] != sucursal[j]){
+                    Empleado = 1;
+            }
+        }
+        if(Empleado > 0 ){
+            cantidadempleados +1;
+        }
+
+
+
+
